435b1f6c942f03fa23260fe7fdcd57738555e1a0
Kamil Halko <k.halko@nomtek.com>
2020-07-28 12:16:47 +0200

    Reproduce Audioburst-Player crash

--- app/src/main/java/com/kamilh/gotcharacters/GoTCharacters.kt
+++ app/src/main/java/com/kamilh/gotcharacters/GoTCharacters.kt
@@ -1,5 +1,7 @@
 package com.kamilh.gotcharacters
 
+import com.audioburst.audioburst_player.AudioburstPlayer
+import com.audioburst.audioburst_player.SdkKeys
 import com.facebook.stetho.Stetho
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.kamilh.gotcharacters.di.DaggerAppComponent
@@ -16,5 +18,13 @@ class GoTCharacters : DaggerApplication() {
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
         }
+
+        AudioburstPlayer.init(
+            context = this,
+            sdkKeys = SdkKeys(
+                applicationKey = "5bea6355b9cd4e78aee3403346617b1c",
+                experienceId = "0823a4b7-3296-45d0-a368-a33411f9c1e0"
+            )
+        )
     }
 }
